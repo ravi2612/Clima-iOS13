8dd1b9511deb5792d7a610012266cd855ee898b5
ravi <ravinavarro26@gmail.com>
2021-02-17 00:14:02 -0300

    initial commit

---
+++ Clima/Models/WeatherManager.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by raji navarro on 15/02/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=ef53df87ac746c87522688ea82936184&units=metric"
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create URL
+        if let url = URL(string: urlString){
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:responde:error:))
+            
+            //4. Start the task
+            
+            task.resume()
+            
+        }
+        
+    }
+    
+    func handle(data: Data?, responde: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
+    }
+}
+
